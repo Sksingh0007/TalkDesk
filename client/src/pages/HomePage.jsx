69641001a761c005c630f8aad39539128b6104a8
Shivam Kumar Singh <sks21062003@gmail.com>
2025-07-17 14:41:51 +0530

    right-side-bar

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,11 +1,12 @@
-import React, { useState } from 'react'
+import React, { useContext, useState } from 'react'
 import Sidebar from '../components/Sidebar'
 import ChatContainer from '../components/ChatContainer'
 import RightSidebar from '../components/RightSidebar'
+import { ChatContext } from '../../context/ChatContext'
 
 const HomePage = () => {
 
-  const [selectedUser, setSelectedUser] = useState(null)
+  const  {selectedUser} = useContext(ChatContext)
 
   return (
     <div className="border w-full h-screen sm:px-[15%] sm:py-[5%]">
@@ -19,10 +20,7 @@ const HomePage = () => {
       >
         <Sidebar/>
         <ChatContainer/>
-        <RightSidebar
-          selectedUser={selectedUser}
-          setSelectedUser={setSelectedUser}
-        />
+        <RightSidebar/>
       </div>
     </div>
   );
