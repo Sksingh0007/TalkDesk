1c4e2bb9d49ff617db373f74da129a5c2c99e8ad
Shivam Kumar Singh <sks21062003@gmail.com>
2025-07-17 16:53:15 +0530

    updated-assets

--- server/server.js
+++ server/server.js
@@ -62,6 +62,7 @@ await connectDB();
 
 const PORT = process.env.PORT || 5000;
 
+
 if (process.env.NODE_ENV !== "production") {
   server.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
